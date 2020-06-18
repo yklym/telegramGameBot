5e269aeadc88d2d0176753d29a7cf67219da40ca
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-06-18 14:39:23 +0300

    config updated with db settings

--- data/config.py
+++ data/config.py
@@ -1,9 +1,15 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 
-# admins = [
-# ]
+DB_CONF = {
+    'host': str(os.getenv("DB_HOST")),
+    'user': str(os.getenv("DB_USER")),
+    'database': str(os.getenv("DB_DATABASE")),
+    'password': str(os.getenv("DB_PASSWORD")),
+    'port': str(os.getenv("DB_PORT"))
+}
