ec966ab57900d4b166e026043a858e08007f94e4
vsymoniuk <izogid42@gmail.com>
2020-06-30 15:08:00 +0300

    add rules text

--- handlers/shared/commands.py
+++ handlers/shared/commands.py
@@ -2,6 +2,7 @@ import requests
 
 from data.config import cat_api_url
 from data.text.default_commands import chat_info_text
+from data.text.rules import get_rules
 from loader import bot
 
 
@@ -14,3 +15,8 @@ def chat_info(message):
 def get_random_cat(message):
     response = requests.get(cat_api_url)
     bot.send_photo(message.chat.id, response.json()[0]["url"])
+
+@bot.message_handler(commands=['rules'])
+def show_rules(message):
+    page = int(message.text.split(' ')[-1]) -1
+    bot.send_message(message.chat.id,get_rules()[page],parse_mode='html')
